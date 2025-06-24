a799535381095da78a43a1df753d6a8f8832442b
Ravi Tiwari <ravitiwari@Ravis-MacBook-Air.local>
2025-06-24 19:49:36 +0530

    adding Tokenizer(encode,decode for tokenids)

---
+++ mytokenizers/Tokenizer.py
@@ -0,0 +1,28 @@
+import re
+
+
+class SimpleTokenizer:
+    def __init__(self, vocab):
+        self.vocab = vocab #total number of words where {int : word}
+        self.rev_vocab = {v:k for k,v in vocab.items()}
+
+    def encode(self,text):
+        words = re.split(r'([,.:;?_!"()\']|--|\s)', text) #split the text into sentances\
+        words = [item.strip() for item in words if item.strip()] # remove the spaces 
+        words = [
+            item for item in words                        # if any word is not present in vocab then putting "<|unk|>" 
+        ]  
+
+        return [self.vocab[s] for s in words]
+
+    def decode(self, ids):
+        text = " ".join([self.rev_vocab[i] for i in ids])
+        # Replace spaces before the specified punctuations
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
+        return text               
+        
+if __name__=="__main__":
+    print("fine")
+
+
+        
