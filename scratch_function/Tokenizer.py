2e8bc2cf9f48053dfa6e3e8c44e707dc2aec81b9
Ravi Tiwari <ravitiwari@Ravis-MacBook-Air.local>
2025-07-28 20:03:06 +0530

    bpe tokenizer

--- scratch_function/Tokenizer.py
+++ scratch_function/Tokenizer.py
@@ -1,17 +1,15 @@
 import re
-
-
+import json
 class SimpleTokenizer:
     def __init__(self, vocab):
-        self.vocab = vocab #total number of words where {int : word}
-        self.rev_vocab = {v:k for k,v in vocab.items()}
+        self.vocab =vocab #total number of words where {int : word}
+        self.rev_vocab = {v:k for k,v in self.vocab.items()}
 
     def encode(self,text):
         words = re.split(r'([,.:;?_!"()\']|--|\s)', text) #split the text into sentances\
         words = [item.strip() for item in words if item.strip()] # remove the spaces 
         words = [
-            item if item in self.vocab 
-            else "<|unk|>" for item in words                        # if any word is not present in vocab then putting "<|unk|>" 
+            item for item in words                        # if any word is not present in vocab then putting "<|unk|>" 
         ]  
 
         return [self.vocab[s] for s in words]
