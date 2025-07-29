5c3537fb380988fc29b0a31f24f4d5130bc122cd
Ravi Tiwari <ravitiwari@Ravis-MacBook-Air.local>
2025-07-29 20:11:11 +0530

    few changes

--- scratch_function/Dataloader.py
+++ scratch_function/Dataloader.py
@@ -4,11 +4,13 @@ from torch.utils.data import Dataset, DataLoader
 
 
 class MiniLLMTokenizer:
-    def __init__(self, txt, tokenizer, context_size, stride):
+    def __init__(self, text, tokenizer, context_size, stride):
         self.input_ids = []
         self.target_ids = []
     
         token_ids = tokenizer.encode(text)
+        context_size= min(len(token_ids)-2,context_size )
+        
         
         for i in range(1, len(token_ids)- context_size, stride):
             input_chunk = token_ids[i:i + context_size]
@@ -25,7 +27,7 @@ class MiniLLMTokenizer:
 def data_loader(text, batch_size =8, context_size=256, stride=128, shuffle = True, drop_last = True, n_workers=0):
 
     tokenizer = tiktoken.get_encoding('gpt2')
-
+    
     dataset = MiniLLMTokenizer(text, tokenizer,context_size,stride=stride)
     dataloader = DataLoader(
             dataset=dataset,
@@ -39,5 +41,5 @@ def data_loader(text, batch_size =8, context_size=256, stride=128, shuffle = Tru
 
 
 if __name__=='__main__':
-    text = 'How are you?'
-    print(print(next(iter(data_loader(text, batch_size=1,context_size=1, stride=1)))))
+    text = 'How are you?, how you doing?, its been so long i havnt seen you'
+    print(print(next(iter(data_loader(text, batch_size=1,context_size=8, stride=1)))))
